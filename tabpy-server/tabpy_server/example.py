62451ec197791336cef9afae6a873f293c119494
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 20:06:18 -0700

    Create example.py

---
+++ tabpy-server/tabpy_server/example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import tabpy_client
+from time import sleep
+
+sleep(10)
+
+client = tabpy_client.Client('http://localhost:9004/')
+
+def clustering(x, y):
+    import numpy as np
+    from sklearn.cluster import DBSCAN
+    from sklearn.preprocessing import StandardScaler
+    X = np.column_stack([x, y])
+    X = StandardScaler().fit_transform(X)
+    db = DBSCAN(eps=1, min_samples=3).fit(X)
+    return db.labels_.tolist()
+
+
+client.deploy('clustering',
+                clustering,
+                'Returns cluster Ids for each data point specified by the pairs in x and y')
