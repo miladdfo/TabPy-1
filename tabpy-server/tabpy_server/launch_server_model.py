86f7e18e33fca1501989739e6d0b045b10213a7b
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 21:23:04 -0700

    Update launch_server_model.py

--- tabpy-server/tabpy_server/launch_server_model.py
+++ tabpy-server/tabpy_server/launch_server_model.py
@@ -5,13 +5,8 @@ import os
 
 
 if sys.platform in ['darwin', 'linux2', 'linux']:
-    if len(sys.argv) >= 2:
-        subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/tabpy.py','--port','9004'])
-        subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/example.py'])
-    else:
-        subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/tabpy.py','--port','9004'])
-        subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/example.py'])
-
+    subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/example.py'])
+    subprocess.call(['python', os.path.dirname(os.path.realpath(__file__))+'/tabpy.py','--port','9004'])
 else:
     print('Operating system not recognized')
 
