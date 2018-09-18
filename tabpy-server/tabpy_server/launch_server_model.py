a1086f018a688e83b3dbb0493433d9fb6b53cc8a
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 20:43:36 -0700

    Create launch_server_model.py

---
+++ tabpy-server/tabpy_server/launch_server_model.py
@@ -0,0 +1,17 @@
+import sys
+import subprocess
+import os
+
+
+if sys.platform in ['darwin', 'linux2', 'linux']:
+    if len(sys.argv) >= 2:
+        subprocess.call(['python', 'tabpy.py','--port','9004'])
+        subprocess.call(['python', 'example.py'])
+    else:
+        subprocess.call(['python', 'tabpy.py','--port','9004'])
+        subprocess.call(['python', 'example.py'])
+
+else:
+    print('Operating system not recognized')
+
+print("I am finished running startup.py!")
