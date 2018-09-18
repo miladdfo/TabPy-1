eae42c4331a3472671d429d14317ddceeef8bb12
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 20:49:10 -0700

    Update launch_server_model.py

--- tabpy-server/tabpy_server/launch_server_model.py
+++ tabpy-server/tabpy_server/launch_server_model.py
@@ -5,11 +5,11 @@ import os
 
 if sys.platform in ['darwin', 'linux2', 'linux']:
     if len(sys.argv) >= 2:
-        subprocess.call(['python', 'tabpy.py','--port','9004'])
-        subprocess.call(['python', 'example.py'])
+        subprocess.call(['python', './tabpy.py','--port','9004'])
+        subprocess.call(['python', './example.py'])
     else:
-        subprocess.call(['python', 'tabpy.py','--port','9004'])
-        subprocess.call(['python', 'example.py'])
+        subprocess.call(['python', './tabpy.py','--port','9004'])
+        subprocess.call(['python', './example.py'])
 
 else:
     print('Operating system not recognized')
