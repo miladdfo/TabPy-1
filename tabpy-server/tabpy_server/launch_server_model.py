b5cc46dc1e52ec0465c6826c325cd5bc879dc262
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 21:36:27 -0700

    Update launch_server_model.py

--- tabpy-server/tabpy_server/launch_server_model.py
+++ tabpy-server/tabpy_server/launch_server_model.py
@@ -6,7 +6,7 @@ import os
 model = "python "+os.path.dirname(os.path.realpath(__file__))+'/example.py'
 server = "python "+os.path.dirname(os.path.realpath(__file__))+'/tabpy.py --port 9004'
 
-p = subprocess.Popen(model , shell=True, stdout=subprocess.PIPE)
-p = subprocess.Popen(server , shell=True, stdout=subprocess.PIPE)
+p1 = subprocess.Popen(model , shell=True, stdout=subprocess.PIPE)
+p2 = subprocess.Popen(server , shell=True, stdout=subprocess.PIPE)
 
 print("I am finished running startup.py!")
