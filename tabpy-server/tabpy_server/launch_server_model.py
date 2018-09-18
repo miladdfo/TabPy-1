84a074cd60481163def83dbd73771758e29f8707
miladdfo <42683063+miladdfo@users.noreply.github.com>
2018-09-17 21:07:22 -0700

    Update launch_server_model.py

--- tabpy-server/tabpy_server/launch_server_model.py
+++ tabpy-server/tabpy_server/launch_server_model.py
@@ -3,11 +3,12 @@ import sys
 import subprocess
 import os
 
+print os.path.realpath(__file__)
 
 if sys.platform in ['darwin', 'linux2', 'linux']:
     if len(sys.argv) >= 2:
-        subprocess.call(['python', './tabpy.py','--port','9004'])
-        subprocess.call(['python', './example.py'])
+        subprocess.call(['python', os.path.realpath(__file__)+'/tabpy.py','--port','9004'])
+        subprocess.call(['python', os.path.realpath(__file__)+'/example.py'])
     else:
         subprocess.call(['python', './tabpy.py','--port','9004'])
         subprocess.call(['python', './example.py'])
